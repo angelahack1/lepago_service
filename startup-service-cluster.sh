511631d9f5f45bd2b390eaaa8f4a54ed7e6149e3
Angela López Mendoza <angelakimichellle@hotmail.com>
2025-05-09 12:12:45 -0600

    Initial commite, angelahack1

---
+++ startup-service-cluster.sh
@@ -0,0 +1,6 @@
+kubectl apply -f kubernetes-compose-01.yaml
+kubectl apply -f kubernetes-compose-02.yaml
+kubectl apply -f kubernetes-compose-03.yaml
+kubectl apply -f kubernetes-compose-04.yaml
+kubectl apply -f kubernetes-compose-05.yaml
+
---
+++ stop-service-cluster.sh
@@ -0,0 +1,5 @@
+kubectl delete -f kubernetes-compose-01.yaml
+kubectl delete -f kubernetes-compose-02.yaml
+kubectl delete -f kubernetes-compose-03.yaml
+kubectl delete -f kubernetes-compose-04.yaml
+kubectl delete -f kubernetes-compose-05.yaml

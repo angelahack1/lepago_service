6e03d523f5c831d38fe68402681b6d975a33cf66
angelahack1 <angelakimichellle@hotmail.com>
2025-06-07 22:56:59 -0600

    fixing db, by angelahack1

--- db/db_access.js
+++ db/db_access.js
@@ -27,24 +27,44 @@ class DBService {
     }
     connect() {
         return __awaiter(this, void 0, void 0, function* () {
-            if (this.client) {
-                yield this.client.connect();
-                this.db = this.client.db(this.dbName);
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'connect()...');
+            try {
+                if (!this.client) {
+                    console.error(`[${(0, timestamp_1.getFormattedTimestamp)()}] MongoDB client not initialized`);
+                    return null;
+                }
+                if (!this.db) {
+                    console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'connect()->Connecting to MongoDB...');
+                    yield this.client.connect();
+                    console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'connect()->Connected to MongoDB');
+                    this.db = this.client.db(this.dbName);
+                    console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'connect()->Database selected: ', this.dbName);
+                }
                 return this.db;
             }
-            else {
+            catch (error) {
+                console.error(`[${(0, timestamp_1.getFormattedTimestamp)()}] Error connecting to MongoDB:`, error);
                 return null;
             }
         });
     }
     close() {
         return __awaiter(this, void 0, void 0, function* () {
-            if (this.client) {
-                yield this.client.close();
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'close()...');
+            try {
+                if (this.client) {
+                    console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'close()->Closing MongoDB client...');
+                    yield this.client.close();
+                    console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'close()->MongoDB client closed');
+                }
+                else {
+                    console.error(`[${(0, timestamp_1.getFormattedTimestamp)()}] Error closing MongoDB client.`);
+                }
             }
-            else {
-                console.error(`[${(0, timestamp_1.getFormattedTimestamp)()}] Error closing MongoDB client.`);
+            catch (error) {
+                console.error(`[${(0, timestamp_1.getFormattedTimestamp)()}] Error closing MongoDB client:`, error);
             }
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...close()');
         });
     }
     isAliasRegistered(alias) {
@@ -69,24 +89,28 @@ class DBService {
     }
     getLoginNameFromIdc(idc) {
         return __awaiter(this, void 0, void 0, function* () {
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'getLoginNameFromIdc()...');
             try {
                 const collection = this.db.collection(this.collectionNameUsuario);
                 const result = yield collection.findOne({ idc: idc });
-                console.log("Alias from IDC: <", result ? result.alias : null, ">");
+                console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'getLoginNameFromIdc()->Alias from IDC: <', result ? result.alias : null, ">");
                 return result ? result.alias : null;
             }
             catch (error) {
                 console.error(`[${(0, timestamp_1.getFormattedTimestamp)()}] Error getting login name from IDC ${idc}:`, error);
+                console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...getLoginNameFromIdc() error');
                 return null;
             }
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...getLoginNameFromIdc() success');
         });
     }
     getSharedSecretFromIdc(idc) {
         return __awaiter(this, void 0, void 0, function* () {
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'getSharedSecretFromIdc()...');
             try {
                 const collection = this.db.collection(this.collectionNameCryptoArtifacts);
                 const result = yield collection.findOne({ idc: idc });
-                console.log("Shared secret from IDC: <", result ? result.shared_secret : null, ">");
+                console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'getSharedSecretFromIdc()->Shared secret from IDC: <', result ? result.shared_secret : null, ">");
                 if (!(result === null || result === void 0 ? void 0 : result.shared_secret))
                     return null;
                 const undecodedSharedSecret = Buffer.from(result.shared_secret, 'base64');
@@ -94,81 +118,91 @@ class DBService {
             }
             catch (error) {
                 console.error(`[${(0, timestamp_1.getFormattedTimestamp)()}] Error getting shared secret from IDC ${idc}:`, error);
+                console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...getSharedSecretFromIdc() error');
                 return null;
             }
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...getSharedSecretFromIdc() success');
         });
     }
     getCatalogoEstadosUsuario() {
         return __awaiter(this, void 0, void 0, function* () {
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'getCatalogoEstadosUsuario()...');
             try {
                 const collection = this.db.collection(this.collectionNameCatalogoEstadosUsuario);
                 const result = yield collection.find({}).toArray();
+                console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'getCatalogoEstadosUsuario()->Catalogo estados usuario: ', result);
                 return result.map((iter) => iter.nombre_estado);
             }
             catch (error) {
                 console.error(`[${(0, timestamp_1.getFormattedTimestamp)()}] Error fetching catalogo estados usuario:`, error);
+                console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...getCatalogoEstadosUsuario() error');
                 return [];
             }
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...getCatalogoEstadosUsuario() success');
         });
     }
     getCatalogoTiposUsuario() {
         return __awaiter(this, void 0, void 0, function* () {
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'getCatalogoTiposUsuario()...');
             try {
                 const collection = this.db.collection(this.collectionNameCatalogoTiposUsuario);
                 const result = yield collection.find({}).toArray();
+                console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'getCatalogoTiposUsuario()->Catalogo tipos usuario: ', result);
                 return result.map((iter) => iter.nombre_tipo);
             }
             catch (error) {
                 console.error(`[${(0, timestamp_1.getFormattedTimestamp)()}] Error fetching catalogo tipos usuario:`, error);
+                console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...getCatalogoTiposUsuario() error');
                 return [];
             }
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...getCatalogoTiposUsuario() success');
         });
     }
     registerCryptoAssets(idcP, publicKey, sharedSecret) {
         return __awaiter(this, void 0, void 0, function* () {
-            console.log('registerCryptoAssets()...');
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'registerCryptoAssets()...');
             try {
                 const collection = this.db.collection(this.collectionNameCryptoArtifacts);
                 yield collection.insertOne({ idc: idcP, public_key: publicKey, shared_secret: sharedSecret });
             }
             catch (error) {
                 console.error(`[${(0, timestamp_1.getFormattedTimestamp)()}] Error registering crypto assets:`, error);
-                console.log('...registerCryptoAssets() error');
+                console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...registerCryptoAssets() error');
                 return false;
             }
-            console.log('...registerCryptoAssets() success');
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...registerCryptoAssets() success');
             return true;
         });
     }
     registerUsuario(usuario) {
         return __awaiter(this, void 0, void 0, function* () {
-            console.log('registerUsuario()...');
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'registerUsuario()...');
             try {
                 const collection = this.db.collection(this.collectionNameUsuario);
                 yield collection.insertOne(usuario);
             }
             catch (error) {
                 console.error(`[${(0, timestamp_1.getFormattedTimestamp)()}] Error saving user in MongoDB:`, error);
-                console.log('...registerUsuario() error');
+                console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...registerUsuario() error');
                 return false;
             }
-            console.log('...registerUsuario() success');
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...registerUsuario() success');
             return true;
         });
     }
     //TODO: add a function to save the user info gotten from metaInfo encrypted with the shared secret
     saveUserInfo(idcP, info) {
         return __awaiter(this, void 0, void 0, function* () {
-            console.log('saveUserInfo()...');
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, 'saveUserInfo()...');
             try {
                 const collection = this.db.collection(this.collectionNameUsuario);
                 yield collection.updateOne({ idc: idcP }, { $set: { info: info } });
             }
             catch (error) {
                 console.error(`[${(0, timestamp_1.getFormattedTimestamp)()}] Error saving user info in MongoDB:`, error);
-                console.log('...saveUserInfo() error');
+                console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...saveUserInfo() error');
             }
-            console.log('...saveUserInfo() success');
+            console.log(`[${(0, timestamp_1.getFormattedTimestamp)()}]`, '...saveUserInfo() success');
         });
     }
 }
